f789c6deaf01cb5f47a37fad7a77fa78bbfc66f4
Gary <gary369880206@gmail.com>
2022-03-19 19:15:46 +0800

    update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-wget -O models.tar.xz https://www.dropbox.com/s/8tldyaiyarfdk0e/models.tar.xz?dl=1
+wget -O models.tar.xz https://www.dropbox.com/s/y5126tetc9fvhgr/models.tar.xz?dl=1
 tar xvf models.tar.xz
